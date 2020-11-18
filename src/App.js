1832d0a0746dbfdcb8f88f08afe4a0408b7ba504
rohaningale96@gmail.com <rohaningale96@gmail.com>
2020-11-18 21:17:38 +0530

    add vehicle done

--- src/App.js
+++ src/App.js
@@ -1,70 +1,99 @@
+import { useState } from 'react';
 import React from 'react';
 import './App.css';
 import Vehicle from './Vehicle/vehicle';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+const App = props => {
+    const [vehicleState, setVehicleState] = useState({
+        vehicles: [
+            { ID: "1", Vehicletype: "Car", Name: "Altroz", Manufacturer: "Tata", Fueltype: "Petrol" },
+            { ID: "2", Vehicletype: "Car", Name: "Seltos", Manufacturer: "Kia", Fueltype: "Diesel" },
+            { ID: "3", Vehicletype: "Car", Name: "Bolero", Manufacturer: "Mahindra", Fueltype: "Diesel" },
+            { ID: "4", Vehicletype: "Car", Name: "Safari", Manufacturer: "Tata", Fueltype: "Diesel" }
 
-        this.state = {
-            vehicles: [
-                { Vehicletype: "Car", Name: "Altroz", Manufacturer: "Tata", Fueltype: "Petrol" },
-                { Vehicletype: "Car", Name: "Seltos", Manufacturer: "Kia", Fueltype: "Diesel" },
-                { Vehicletype: "Car", Name: "Ballero", Manufacturer: "Mahindra", Fueltype: "Diesel" },
-                { Vehicletype: "Car", Name: "Safari", Manufacturer: "Strome", Fueltype: "Petrol" },
-            ]
-        };
+        ],
+        Count: 4,
+        showvehicles: true,
+        showvehicleslabel: "hide vehicles"
+    })
+
+
+    const addvehiclehandler = () => {
+        const vehiclelist = [...vehicleState.vehicles];
+        const newvehicle = { Vehicletype: "Car", Name: "Nexon", Manufacturer: "Tata", Fueltype: "Electric" }
+        vehiclelist.push(newvehicle);
+        setVehicleState({
+            vehicles: vehiclelist,
+            count: vehicleState.count + 1,
+            showvehicles: vehicleState.showvehicles,
+            showvehicleslabel: vehicleState.showvehicleslabel
+        })
+    }
+    const deletevehiclehandler = (vehicleIndex) => {
+        const vehiclelist = [...vehicleState.vehicles];
+        vehiclelist.slice(vehicleIndex, 1);
+        setVehicleState({
+            vehicles: vehiclelist,
+            count: vehicleState.count - 1,
+            showvehicles: vehicleState.showvehicles,
+            showvehicleslabel: vehicleState.showvehicleslabel
+        })
+
     }
 
-    updatehandler = () => {
-        this.state = {
-            vehicles: [
-                { Vehicletype: "Car", Name: "Altroz", Manufacturer: "Tata", Fueltype: "Petrol" },
-                { Vehicletype: "Car", Name: "Seltos", Manufacturer: "Kia", Fueltype: "Diesel" },
-                { Vehicletype: "Car", Name: "Ballero", Manufacturer: "Mahindra", Fueltype: "Diesel" },
-                { Vehicletype: "Car", Name: "Safari", Manufacturer: "Strome", Fueltype: "Petrol" },
-            ]
-        };
+    const togglevehiclelisthandler = () => {
+        let togglevehicle = !vehicleState.showvehicles;
+        let label = "";
+
+        if (togglevehicle) {
+            label = "Hide Vehicle";
+        } else {
+            label = "show vehicles";
+        }
+        setVehicleState({
+            Vehicles: vehicleState.vehicles,
+            Count: vehicleState.Count,
+            showvehicles: togglevehicle,
+            showvehicleslabel: label
+        })
     }
-
-
-    render() {
-        return ( <
-            div className = "App container-fluid" >
-            <
-            div className = "row" >
-            <
-            h1 > List of Vehicles. < /h1> <
-            button className = "btn btn-primary btn buttonDiv buttonUpdate"
-            onClick = { this.updatehandler } > UPDATE LIST < /button> <
-            /div> <
-            div className = "row" >
-            <
-            Vehicle Vehicletype = { this.state.vehicles[0].Vehicletype }
-            Name = { this.state.vehicles[0].Name }
-            Manufacturer = { this.state.vehicles[0].Manufacturer }
-            Fueltype = { this.state.vehicles[0].Fueltype }
-            /> <
-            Vehicle Vehicletype = { this.state.vehicles[1].Vehicletype }
-            Name = { this.state.vehicles[1].Name }
-            Manufacturer = { this.state.vehicles[1].Manufacturer }
-            Fueltype = { this.state.vehicles[1].Fueltype }
-            /> <
-            Vehicle Vehicletype = { this.state.vehicles[2].Vehicletype }
-            Name = { this.state.vehicles[2].Name }
-            Manufacturer = { this.state.vehicles[2].Manufacturer }
-            Fueltype = { this.state.vehicles[2].Fueltype }
-            /> <
-            Vehicle Vehicletype = { this.state.vehicles[3].Vehicletype }
-            Name = { this.state.vehicles[3].Name }
-            Manufacturer = { this.state.vehicles[3].Manufacturer }
-            Fueltype = { this.state.vehicles[3].Fueltype }
-            /> <
-            /div> <
+    let vehiclelist = null;
+    if (vehicleState.showvehicles) {
+        vehiclelist = ( < div className = "row" > {
+                vehicleState.vehicles.map((vehicleItem, index) => {
+                    return <Vehicle key = { vehicleItem.ID }
+                    Vehicletype = { vehicleItem.Vehicletype }
+                    Name = { vehicleItem.Name }
+                    Manufacturer = { vehicleItem.Manufacturer }
+                    Fueltype = { vehicleItem.Fueltype }
+                    onDelete = { deletevehiclehandler.bind(this, index) }
+                    />
+                })
+            } <
             /div>
-        );
-    };
+        )
+    }
 
-}
+    return ( <
+        div className = "App container-fluid" >
+        <
+        div className = "row" >
+        <
+        div className = "col-lg-6" >
+        <
+        h1 > List of Vehicles | Total vehicle: { vehicleState.Count } < /h1> <
+        /div> <
+        div className = "col-lg-6 divbutton" >
+        <
+        button className = "btn btn-primary btn buttonUpdate"
+        onClick = { addvehiclehandler } > ADD VEHICLE < /button> <
+        button className = "btn btn-secondary btn buttonUpdate"
+        onClick = { togglevehiclelisthandler } > { vehicleState.showvehicleslabel } < /button> <
+        /div>
 
+        <
+        /div> { vehiclelist } <
+        /div>
+    )
+};
 export default App;
--- src/Vehicle/vehicle.js
+++ src/Vehicle/vehicle.js
@@ -1,16 +1,16 @@
 import './vehicle.css'
 
 const vehicle = props => ( <
-    div className = 'vehicle' >
+    div className = "vehicle col-lg-3" >
     <
     h1 >
     vehicle: { props.Vehicletype } <
     /h1> <
-    p > Name: { props.Name } < /p> <
+    p onClick = { props.updateclick } > Name: { props.Name } < /p> <
     p > Manufacturer: { props.Manufacturer } < /p> <
-    p > Fuel: { props.Fueltype } < /p>
-
-    <
+    p > Fuel: { props.Fueltype } < /p> <
+    button className = "btn btn-danger btn-sm"
+    onClick = { props.onDelete } > Delete < /button> <
     /div>
 );
 export default vehicle;
